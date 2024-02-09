368f040436668f83b28205f8c6d8e8d1b7709d3d
HunaisAshraf <hunaisashraf@gmail.com>
2024-02-09 20:35:55 +0530

    sorting Algorithm

---
+++ section 2/sorting  algorithm/sortAlgorithm.js
@@ -0,0 +1,9 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        
+    }
+  }
+}
+
+console.log(bubbleSort([4, 2, 3, 6, 7, 8, 1]));
